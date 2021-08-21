30f8924ec71b408e84ea3fb6c09a24101cb9fe4c
Manjado <michalalchimowicz@gmail.com>
2021-08-21 10:19:09 +0200

    fetchApi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import type { InferGetStaticPropsType } from "next"
+import getAllProducts from "../framework/shopify/product/get-all-products"
+
+export async function getStaticProps() {
+  const products = await getAllProducts()
+  console.log("TETST")
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  }
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const message: string = "Hello World"
 
-  return <div>{message}</div>
+  return <div>{JSON.stringify(products)}</div>
 }
