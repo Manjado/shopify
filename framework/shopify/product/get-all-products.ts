dd32bd2821ddeb14c9debb0f18ed1e4c0e56f5c0
Manjado <michalalchimowicz@gmail.com>
2021-08-22 11:46:37 +0200

    normalize product and image

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,17 +1,22 @@
-import getAllProductsQuery from "../utils/queries/get-all-products"
 import fetchApi from "../utils/fetch-api"
+import getAllProductsQuery from "../utils/queries/get-all-products"
+import { normalizeProduct } from "../utils/normalize"
 import { ProductConnection } from "../schema"
+import { Product } from "../../common/types/product"
 
 type ReturnData = {
   products: ProductConnection
 }
 
-const getAllProducts = async (): Promise<any> => {
+const getAllProducts = async (): Promise<Product[]> => {
   const { data } = await fetchApi<ReturnData>({
     query: getAllProductsQuery,
   })
 
-  const products = data.products.edges.map(({ node: product }) => product) ?? []
+  const products =
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+    []
+
   return products
 }
 
