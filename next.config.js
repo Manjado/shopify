1fe5e63f31407ddfaf955added5df4e7897363cd
Manjado <michalalchimowicz@gmail.com>
2021-08-24 20:03:11 +0200

    re-usable configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+const { withFrameworkConfig } = require("./framework/common/config")
+
+// module.exports = {
+//   reactStrictMode: true,
+//   i18n: {
+//     locales: ["en-US", "es"],
+//     defaultLocale: "en-US",
+//   },
+// }
+
+module.exports = withFrameworkConfig({
   reactStrictMode: true,
-}
+  framework: {
+    name: "shopify",
+  },
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+})
