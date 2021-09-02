45be0bb39311fbde49bf7f7e4960adf4ec0b5b4b
Manjado <michalalchimowicz@gmail.com>
2021-09-02 20:38:43 +0200

    marquee ticker

---
+++ components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, FC } from "react"
+import Ticker from "react-ticker"
+import s from "./Marquee.module.css"
+
+interface Props {
+  children: ReactNode[]
+}
+
+const Marquee: FC<Props> = ({ children }) => {
+  return (
+    <div className={s.root}>
+      <Ticker offset={80}>
+        {() => <div className={s.container}>{children}</div>}
+      </Ticker>
+    </div>
+  )
+}
+
+export default Marquee
