c53b2b9bdfa2c81c514c851f10150915f188d011
Manjado <michalalchimowicz@gmail.com>
2021-09-05 16:22:45 +0200

    Layout B and UI

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react"
 import style from "./Layout.module.css"
+import { Footer, Navbar } from "@components/common"
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={style.root}>
+      <Navbar />
       <main className="fit">{children}</main>
+      <Footer />
     </div>
   )
 }
