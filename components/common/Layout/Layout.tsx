84e58e62f83c7377e126df28a9c682075f2f1b01
Manjado <michalalchimowicz@gmail.com>
2021-09-06 11:56:20 +0200

    sidebars, wishlist & ui provider

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,19 @@
 import { FC } from "react"
 import style from "./Layout.module.css"
 import { Footer, Navbar } from "@components/common"
+import { Sidebar } from "@components/ui"
+import { CartSidebar } from "@components/cart"
+import { useUI } from "@components/ui/context"
 
 const Layout: FC = ({ children }) => {
+  const ui = useUI()
+  console.log("UI: ", ui)
   return (
     <div className={style.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">{children}</main>
       <Footer />
     </div>
