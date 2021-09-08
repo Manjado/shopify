72de0a643e8fafe3d86033b63a512f13783091f8
Manjado <michalalchimowicz@gmail.com>
2021-09-08 18:50:15 +0200

    useReducer

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, useContext, useState } from "react"
+import { createContext, FC, useContext, useReducer } from "react"
 
 export interface StateModifires {
   openSidebar: () => void
@@ -24,15 +24,35 @@ const UIContext = createContext<State>({
   ...stateModifires,
   ...initialState,
 })
+type Action = { type: "OPEN_SIDEBAR" | "CLOSE_SIDEBAR" }
+
+function uiReducer(state: StateValues, action: Action) {
+  switch (action.type) {
+    case "OPEN_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: true,
+      }
+    }
+    case "CLOSE_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: false,
+      }
+    }
+  }
+}
 
 export const UIProvider: FC = ({ children }) => {
-  const openSidebar = () => alert("opening sidebar")
-  const closeSidebar = () => alert("Closing sidebar")
+  const [state, dispatch] = useReducer(uiReducer, initialState)
+
+  const openSidebar = () => dispatch({ type: "OPEN_SIDEBAR" })
+  const closeSidebar = () => dispatch({ type: "CLOSE_SIDEBAR" })
 
   const value = {
+    ...state,
     openSidebar,
     closeSidebar,
-    isSidebarOpen: false,
   }
 
   return <UIContext.Provider value={value}>{children}</UIContext.Provider>
