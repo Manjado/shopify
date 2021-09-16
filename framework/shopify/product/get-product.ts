79f16b61d6da68d09ea473b5b315090976fcceee
Manjado <michalalchimowicz@gmail.com>
2021-09-16 22:11:41 +0200

    get-product query

--- framework/shopify/product/get-product.ts
+++ framework/shopify/product/get-product.ts
@@ -1,6 +1,18 @@
 import { ApiConfig } from "@common/types/api"
+import { getProductQuery } from "@framework/utils"
 
-const getPorduct = async (config: ApiConfig): Promise<any> => {
+const getPorduct = async (options: {
+  config: ApiConfig
+  variables: any
+}): Promise<any> => {
+  const { config, variables } = options
+  const { data } = await config.fetch<any>({
+    query: getProductQuery,
+    url: config.apiUrl,
+    variables,
+  })
+
+  console.log(JSON.stringify(data, null, 2))
   return {
     product: {
       name: "My super product",
