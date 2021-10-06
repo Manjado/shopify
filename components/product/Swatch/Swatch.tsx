87658f5fd303c38480a75e1afb8ddfa9e9874a67
Manjado <michalalchimowicz@gmail.com>
2021-10-06 21:19:18 +0200

    active state

--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -1,24 +1,39 @@
 import { FC } from "react"
 import s from "./Swatch.module.css"
+import cn from "classnames"
+import { Check } from "@components/icons"
 
 interface Props {
   color?: string
   label?: string
+  active?: boolean
   variant?: "size" | "color" | string
   onClick: () => void
 }
 
-const Swatch: FC<Props> = ({ color, label, variant, ...rest }) => {
+const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
   console.log(label, "lab")
   label = label?.toLowerCase()
   variant = variant?.toLowerCase()
 
+  const rootClassName = cn(s.root, {
+    [s.active]: active,
+    [s.color]: color,
+    [s.size]: variant === "size",
+  })
+
   return (
     <button
       style={color ? { backgroundColor: color } : {}}
-      className={s.root}
+      className={rootClassName}
       {...rest}
     >
+      {variant === "color" && active && (
+        <span>
+          <Check />
+        </span>
+      )}
+
       {variant === "size" ? label : null}
     </button>
   )
